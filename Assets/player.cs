26fcb0c2f3d5b204f5bc0ff11e07034df1418b00
RZNewman <father.anger@gmail.com>
2018-10-12 21:53:52 -0500

    player movement

---
+++ Assets/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    Rigidbody rb;
+    CharacterController cc;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
+	}
+    const float speed = 2.5f;
+	// Update is called once per frame
+	void Update () {
+        Vector3 h = Input.GetAxis("Horizontal") * Vector3.right;
+        Vector3 v  = Input.GetAxis("Vertical") * Vector3.forward;
+        Vector3 vel = (h + v).normalized * speed*Time.deltaTime;
+        cc.Move(vel);
+    }
+}
