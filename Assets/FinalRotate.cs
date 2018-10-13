846ce30ed465fbf27d224b96302bd8cfc8b53335
RZNewman <father.anger@gmail.com>
2018-10-13 15:46:34 -0500

    final rotation, manny texture

---
+++ Assets/FinalRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalRotate : MonoBehaviour {
+    bool triggered = false;
+    float rotation = 0;
+    float start;
+	// Use this for initialization
+	void Start () {
+        start = transform.rotation.eulerAngles.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (triggered & rotation < 180)
+        {
+            rotation += 1;
+            transform.rotation = Quaternion.Euler(0, start + rotation, 0);
+        }
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered)
+        {
+            triggered = true;
+        }
+    }
+}
